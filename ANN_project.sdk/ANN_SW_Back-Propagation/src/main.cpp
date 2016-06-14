daa1afc9aefbb8a3005a225df9568f0c583e176d
pemsac <100292343@alumnos.uc3m.es>
2016-06-15 01:04:33 +0200

    backup

---
+++ ANN_project.sdk/ANN_SW_Back-Propagation/src/main.cpp
@@ -0,0 +1,17 @@
+/**
+ *
+ * Carlos III University of Madrid.
+ *
+ * Master Final Thesis: Heartbeat classifier based on ANN (Artificial Neural
+ * Network).
+ *
+ * Author: Pedro Marcos Solórzano
+ * Tutor: Luis Mengibar Pozo (Tutor)
+ *
+ *
+ */
+
+
+int main(){
+  return 0;
+}
